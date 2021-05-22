d69deb072c2d17635de26fc92c95070329e388ea
nata654s <nata654s@yahoo.com>
2021-05-22 10:58:46 +0300

    added views and views mixins

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 # Create your models here.
@@ -39,11 +40,15 @@ class Module(models.Model):
     course = models.ForeignKey(Course,
                                related_name='modules',
                                on_delete=models.CASCADE)
-    title= models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'], null=True)
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
@@ -60,6 +65,10 @@ class Content(models.Model):
                                      on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'], null=True)
+
+    class Meta:
+        ordering = ['order']
 
 
 
